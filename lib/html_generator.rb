a2ba23bcf4535172e910fe8591668f6b27e12cae
Jerzy Sładkowski <jerzy.sladkowski@gmail.com>
2014-11-25 03:00:20 +0100

    Cumulative update. Basic functionality was added, normal load of email content in iframe.

--- lib/html_generator.rb
+++ lib/html_generator.rb
@@ -4,6 +4,11 @@ class HtmlGenerator
     define_context
   end
 
+  def generate_all
+    index_pages
+    folder_index_pages
+  end
+
   def index_pages
     folders = Folder.roots.order(:name)
     file = "#{@env.arch_path}/index.html"
@@ -18,19 +23,38 @@ class HtmlGenerator
 
   def folder_index_pages
     folders = Folder.all
+    jspath = @env.arch_path + "/assets/js/pathes.js"
+    @js = File.open(jspath, "w+b", 0644)
+    @js.write "var folder_path=[];\n"
     folders.each do |folder|
       path = ([@env.arch_path] + folder.ancestry_path).join('/')
+      @js.write "folder_path['folder_#{folder.id}']='#{URI.escape folder.ancestry_path.join('/')}/'\n"
+      if folder.messages.count > 1
+        first = folder.messages.order(:created_at).first.created_at.to_date
+        last = folder.messages.order(:created_at).last.created_at.to_date
+        time_range = first..last
+      end
       file = "#{path}/threaded.html"
-      File.open(file, "w+b", 0644) {|f| f.write @view.render(template: "threaded", :locals => { :folder => folder })}
+      File.open(file, "w+b", 0644) {|f| f.write @view.render(template: "threaded",
+                                                             locals: { folder: folder, time_range: time_range })}
       file = "#{path}/date.html"
-      File.open(file, "w+b", 0644) {|f| f.write @view.render(template: "date", :locals => { :folder => folder })}
+      messages = folder.messages.order(:created_at)
+      File.open(file, "w+b", 0644) {|f| f.write @view.render(template: "date",
+                                                             locals: { folder: folder, time_range: time_range, messages: messages })}
       file = "#{path}/subject.html"
-      File.open(file, "w+b", 0644) {|f| f.write @view.render(template: "subject", :locals => { :folder => folder })}
+      messages = folder.messages.order(:subject)
+      File.open(file, "w+b", 0644) {|f| f.write @view.render(template: "subject",
+                                                             locals: { folder: folder, time_range: time_range, messages: messages })}
       file = "#{path}/author.html"
-      File.open(file, "w+b", 0644) {|f| f.write @view.render(template: "author", :locals => { :folder => folder })}
+      messages = folder.messages.order(:from)
+      File.open(file, "w+b", 0644) {|f| f.write @view.render(template: "author",
+                                                             locals: { folder: folder, time_range: time_range, messages: messages })}
       file = "#{path}/attachment.html"
-      File.open(file, "w+b", 0644) {|f| f.write @view.render(template: "attachment", :locals => { :folder => folder })}
+      messages = folder.messages.where(has_attachment: true).order(:created_at)
+      File.open(file, "w+b", 0644) {|f| f.write @view.render(template: "attachment",
+                                                             locals: { folder: folder, time_range: time_range, messages: messages })}
     end
+    @js.close
   end
 
 end
